8a4d8e27d59b20e19090ce492a8eca29451d8475
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2026-04-17 18:03:52 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.scss";
 import App from "./app";
+import Admin from "./components/Admin";
 import { Provider } from "./context";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <Routes>
-      <Route
-        path="*"
-        element={
-          <Provider>
-            <App />
-          </Provider>
-        }
-      />
-    </Routes>
+    <Provider>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Provider>
   </BrowserRouter>,
 );
