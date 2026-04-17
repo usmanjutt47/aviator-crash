f6e8252af4e0533a996a9001b1f5f52dd8d8ee6e
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2026-04-17 15:25:18 +0500

    Update index.tsx

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { toast } from "react-hot-toast";
 import "./toast.scss";
 
 const toaster = (
@@ -7,44 +6,10 @@ const toaster = (
   msg: string,
   currency?: string,
   point?: number,
-  cashoutAmount?: number
+  cashoutAmount?: number,
 ) => {
-  switch (type) {
-    case "success":
-      toast.custom(
-        (t) => (
-          <div className="alert-win celebrated ng-trigger ng-trigger-messageState ng-star-inserted ng-animating">
-            <div className="multiplier ">
-              <div className="label ">{msg}</div>
-              <div className="value ">{point}x</div>
-            </div>
-            <div className="win celebrated">
-              <div className="label no-wrap ">
-                <span>Win</span>
-                <span className="currency ">, {currency}</span>
-              </div>
-              <div className="value ">
-                <span>{cashoutAmount}</span>
-              </div>
-            </div>
-            <button
-              type="button"
-              className="close"
-              onClick={() => { toast.dismiss(t.id); }}
-            >
-              ×
-            </button>
-          </div>
-        )
-      );
-      break;
-    case "error":
-      toast.error(msg);
-      break;
-
-    default:
-      break;
-  }
+  // All toast notifications have been disabled.
+  return;
 };
 
 export default toaster;
